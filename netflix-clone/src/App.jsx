9d4cedc4a3115449da71aa8a6843e737fb71d7a9
myGitHubAccount <muyakedir@gmail.com>
2025-08-19 09:00:41 +0300

    header and footer

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+import React from 'react'
+import Home from './Pages/Home/Home';
+
+const App = () => {
+  return (
+    <div>
+     <Home />
+     
+    </div>
+  );
+}
+
+export default App
+
